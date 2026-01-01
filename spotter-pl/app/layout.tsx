06b4b9f839b0e69c9e1a17e969ab67d38cac043a
DAVIN DO <davin4do@gmail.com>
2026-01-01 21:54:02 +0800

    added vercel analytics to layout

--- spotter-pl/app/layout.tsx
+++ spotter-pl/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const inter = Inter({
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en" data-theme="dark">
       <body className={inter.className}>{children}</body>
+      <Analytics />
     </html>
   );
 }
